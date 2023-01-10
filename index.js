feb60423a9528a3368dda2054cf6363416c53dfa
Duy Le <dle2@oberlin.edu>
2023-01-10 15:37:22 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+// database
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+// functions
+app.get('/info', (request, response) => {
+    response.send(`<div>Phonebook has info for ${persons.length} people.</div>
+    <div>${new Date()}</div>`)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(persons.id !== id)
+
+    response.status(204).end()
+})
+
+
+// set up
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
